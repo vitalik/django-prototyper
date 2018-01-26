c3d60a1504b5d15fc89ba557544224497ee47df3
Vitaliy Kucheriavyi <ppr.vitaly@gmail.com>
2018-01-26 17:42:02 +0200

    saving settings

--- frontend/src/components/settings/index.vue
+++ frontend/src/components/settings/index.vue
@@ -1,15 +1,68 @@
 <template>
-    <div>
-        <h2>Settings</h2>
-        asdf asdf
+    <div class="settings">
+        <h2>Django Settings</h2>
+        <hr>
+        <table>
+
+            <tr>
+                <th>USE_I18N = </th>
+                <td> <InputTrueFalse v-model.boolean="settings.USE_I18N" class="form-control" /></td>
+            </tr>
+            <tr>
+                <th>USE_L10N = </th>
+                <td> <InputTrueFalse v-model.boolean="settings.USE_L10N" class="form-control" /></td>
+            </tr>
+            <tr>
+                <th>EMAIL_SUBJECT_PREFIX = </th>
+                <td> <input v-model.lazy="settings.EMAIL_SUBJECT_PREFIX" class="form-control"></td>
+            </tr>
+            <tr>
+                <th>ADMIN_SITE_HEADER = </th>
+                <td> <input v-model.lazy="settings.ADMIN_SITE_HEADER" class="form-control"></td>
+            </tr>
+            <tr>
+                <th>DEFAULT_FROM_EMAIL = </th>
+                <td> <input v-model.lazy="settings.DEFAULT_FROM_EMAIL" class="form-control"></td>
+            </tr>
+            <tr>
+                <th>TIME_ZONE = </th>
+                <td> <input v-model.lazy="settings.TIME_ZONE" class="form-control"></td>
+            </tr>
+            <tr>
+                <th>LANGUAGE_CODE = </th>
+                <td> <input v-model.lazy="settings.LANGUAGE_CODE" class="form-control"></td>
+            </tr>
+        </table>
+        
+
     </div>
 </template>
 
 <script>
+import { store } from '../../store'
+import InputTrueFalse from '../utils/InputTrueFalse'
 
 export default {
     name: 'settings',
+    data() {
+        return {
+            settings: store.project.settings
+        }
+    },
+    components: {
+        InputTrueFalse,
+    }
+}
+</script>
 
+<style lang="scss">
+
+.settings {
+    table th {
+        font-weight: normal;
+        text-align: right;
+        padding-right: 4px;
+    }
 }
 
-</script>
+</style>
---
+++ frontend/src/components/utils/InputTrueFalse.vue
@@ -0,0 +1,24 @@
+<template>
+
+    <select v-model.boolean="selection" @change="$emit('input', selection)">
+        <option :value="true">True</option>
+        <option :value="false">False</option>
+    </select>
+
+</template>
+
+<script>
+
+export default {
+    name: 'InputTrueFalse',
+    props: ['value'],
+    data() {
+        return {
+            selection: false,
+        }
+    },
+    mounted() {
+        this.selection = this.value
+    }
+}
+</script>
