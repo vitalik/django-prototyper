b0ecc16964131551b3bb6b7226819a65c3253687
Vitaliy Kucheriavyi <ppr.vitaly@gmail.com>
2018-02-20 18:07:12 +0200

    refactoring + meta scratch

---
+++ frontend/src/components/appsmodels/MetaEditor.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="card">
+        <strong class="card-header">Meta</strong>
+        <div class="card-body">
+            <pre>
+abstract
+proxy
+managed
+
+
+ordering
+order_with_respect_to
+get_latest_by
+
+indexes
+unique_together
+index_together
+
+
+verbose_name
+verbose_name_plural
+
+
+app_label
+db_table
+
+
+base_manager_name
+
+db_tablespace
+default_manager_name
+default_related_name
+
+            </pre>
+        </div>
+    </div>
+</template>
+
+<script>
+
+
+
+export default {
+    name: 'meta-editor',
+    props: {
+        model: {
+            required: true,
+            type: Object
+        }
+    }
+}
+</script>
+
--- frontend/src/components/appsmodels/ModelEditor.vue
+++ frontend/src/components/appsmodels/ModelEditor.vue
@@ -2,17 +2,60 @@
     <div class="settings">
         <h2>{{ app.name }}.{{ model.name }}</h2>
         <hr>
-        <fields-editor :model="model"></fields-editor>
+        <div class="row">
+            <div class="col-6">
+                <table class="table table-sm table-striped ">
+                    <draggable v-model="model.fields" element="tbody">
+                        <field v-for="(field, pos) in model.fields" 
+                            @activate="on_field_activate"
+                            @activatenext="on_field_next"
+                            @activateprev="on_field_prev"
+                            :class="{'table-primary': active_field && active_field.name == field.name}"
+                            :field="field" 
+                            :pos="pos"
+                            :id="'f_' + pos"
+                            :key="pos">
+                        </field>
+                    </draggable>
+                </table>
+                
+                <pattern-input 
+                    @save="add_field"
+                    style="width: 250px;"
+                    placeholder="Type field..."
+                    btnlabel="Add"
+                    :regExp="/^[a-z][a-z0-9_]*$/i">
+                </pattern-input>
+
+                <meta-editor :model="model" class="mt-4" />
+            </div>
+            <div class="col-6 col-lg-4">
+                <div v-if="active_field" class="card">
+                    <div class="card-header"><strong>{{active_field.name}}</strong> = {{active_field.type}}</div>
+                    <attrs-editor :field="active_field" class="card-body"></attrs-editor>
+                </div>
+            </div>
+        </div>
     </div>
+
 </template>
 
 <script>
 import { store } from '../../store'
-import FieldsEditor from './FieldsEditor'
-
+import _ from 'lodash'
+import draggable from 'vuedraggable'
+import AttrsEditor from './AttrsEditor'
+import Field from './Field'
+import MetaEditor from './MetaEditor'
+import PatternInput from '../utils/PatternInput'
 
 export default {
     name: 'modeleditor',
+    data() {
+        return {
+            active_field: null,
+        }
+    },
     computed: {
         app() {
             return store.app_get(this.$route.params.app)
@@ -21,9 +64,34 @@ export default {
             return store.models_get(this.app.name, this.$route.params.model)
         }
     },
+    methods: {
+        add_field(name) {
+            if (_.find(this.model.fields, {name}) !== undefined) {
+                alert(`Field "${name}" already exist.`)
+                return
+            }
+            this.active_field = store.fields_add(this.model.fields, name)
+        },
+        on_field_activate(field) {
+            this.active_field = field
+        },
+        on_field_next(pos) {
+            let ind = pos + 1
+            if (ind < this.model.fields.length)
+                document.querySelector(`#f_${ind} input`).focus()
+        },
+        on_field_prev(pos) {
+            let ind = pos - 1
+            if (ind >= 0)
+                document.querySelector(`#f_${ind} input`).focus()
+        }
+    },
     components: {
-        FieldsEditor,
+        draggable,
+        AttrsEditor,
+        Field,
+        PatternInput,
+        MetaEditor,
     }
 }
 </script>
-
