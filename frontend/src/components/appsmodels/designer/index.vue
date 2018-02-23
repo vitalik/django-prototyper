6036a3040b5723b7936b9403227caefd1b1c26c8
Vitaliy Kucheriavyi <ppr.vitaly@gmail.com>
2018-02-23 17:34:14 +0200

    WIP: selecting models in designer

--- frontend/src/components/appsmodels/designer/index.vue
+++ frontend/src/components/appsmodels/designer/index.vue
@@ -6,14 +6,17 @@
         </modal>
         
         <div class="card-header">
+            {{selected_models}}
             <button @click="edit_colors=true" class="btn btn-secondary float-right ml-2">colors</button>
             <button @click="autosort" class="btn btn-secondary float-right">Auto sort</button>
         </div>
         <div class="card-body designer h-100">
-            
+
             <model v-for="item in models" 
+                    @click.native="select_model(item, $event)"
                     class="model"
-                    :key="item.app.name + '.' + item.model.name"
+                    :class="{selected:item.selected}"
+                    :key="item.key"
                     :model="item.model"
                     :app="item.app"
                     :style="{left: item.model.ui_left+'px', top: item.model.ui_top+'px'}" >
@@ -35,6 +38,7 @@ export default {
     data() {
         return {
             edit_colors: false,
+            selected_models: [],
         }
     },
     computed: {
@@ -50,7 +54,8 @@ export default {
                         this.$set(m, 'ui_top', n * 40)
                         this.$set(m, 'ui_left', n * 40)
                     }
-                    result.push({app: app, model: m})
+                    let key = app.name + '.' + m.name
+                    result.push({app: app, model: m, selected: _.indexOf(this.selected_models, key) != -1, key:key})
                     n += 1
                 })
             })
@@ -77,6 +82,13 @@ export default {
                     y += 20
                 }
             })
+        },
+        select_model(item, event) {
+            let modifier = event.shiftKey || event.altKey || event.ctrlKey
+            if (!modifier)
+                this.selected_models = [item.key]
+            else
+                this.selected_models.push(item.key)
         }
     },
     components: {
@@ -99,6 +111,10 @@ export default {
         position: absolute;
         min-width: 150px;
         border-radius: 2px;
+    }
+    .designer .model.selected {
+        border: 1px solid #00f !important;
+        box-shadow: 0px 0px 4px #00f;
     }
     .designer .model.active {
         box-shadow: 1px 1px 4px", "#ccc;
