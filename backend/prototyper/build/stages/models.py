24dfe2085b0a7c0200bca62c10aae47de05dd5f1
Vitaliy Kucheriavyi <ppr.vitaly@gmail.com>
2018-03-02 16:18:11 +0200

    some more for model builder

--- backend/prototyper/build/stages/models.py
+++ backend/prototyper/build/stages/models.py
@@ -2,6 +2,21 @@ from ..base import BuildStage
 from pathlib import Path
 
 
+class codelines(list):
+    def __init__(self, initial=None):
+        super(codelines, self).__init__(initial or [])
+    
+    def extend_indent(self, lines, indent=1):
+        spaces = ' ' * (4 * indent)
+        for l in lines:
+            self.append(spaces + l)
+
+
+def camel_to_spaces(s):
+    import re
+    return re.sub("([a-z])([A-Z])", "\g<1> \g<2>", s)
+
+
 class ModelsStage(BuildStage):
     def run(self):
         for app in self.build.details['apps']:
@@ -10,10 +25,61 @@ class ModelsStage(BuildStage):
     def _handle_app(self, app):
         mdodels_py = Path(self.build.build_path) / app['name'] / 'models.py'
 
-        contents = ['from django.db import models', '', '']
+        contents = ['from django.db import models']
+
         for model in app['models']:
-            contents.append('class %s(models.Model):' % model['name'])
-            contents.append('    pass')
             contents.extend(['', ''])
+            model_lines = ModelBuilder(app, model)
+            contents.extend(model_lines)
+        
+        contents.append('') # last empty line
+
+        self.log('\n'.join(contents))
 
         mdodels_py.write_text('\n'.join(contents))
+
+
+class ModelBuilder(codelines):
+    def __init__(self, app, model):
+        super(ModelBuilder, self).__init__()
+        self.app, self.model = app, model
+        self._create()
+
+    def _create(self):
+        self.append('class %s(models.Model):' % self.model['name'])
+
+        for field in self.model['fields']:
+            self.extend_indent(self._handle_field(field))
+
+        self.extend_indent(self._handle_meta())
+        
+        if len(self) == 1:
+            self.append('    pass')
+    
+    def _handle_field(self, field):
+        lines = []
+        lines.append('%s = %s()' % (field['name'], field['type']))
+        return lines
+
+    def _handle_meta(self):
+        lines = codelines([''])
+        lines.append('class Meta:')
+
+        v_name = camel_to_spaces(self.model['name']).lower()
+        v_name_plural = v_name
+        if v_name_plural.endswith('y'):
+            v_name_plural = v_name_plural[:-1] + 'ies'
+        elif not v_name_plural.endswith('s'):
+            v_name_plural += 's'
+        lines.extend_indent(["verbose_name = %s" % self._trans_str(v_name)])
+        lines.extend_indent(["verbose_name_plural = %s" % self._trans_str(v_name_plural)])
+        
+        return lines
+    
+    def _trans_str(self, s):
+        "Returns either _('<s>' or '<s>' based on build settings)"
+        q = "'" in s and '"' or "'"
+        result = q + s + q
+        if True:
+            return '_(' + result + ')'
+        return result
