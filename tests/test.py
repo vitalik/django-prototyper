87c4d93335fe8b0125678a7793b758df5b19162d
Vitaliy Kucheriavyi <ppr.vitaly@gmail.com>
2018-03-03 01:29:03 +0200

    tests

--- tests/test.py
+++ tests/test.py
@@ -8,4 +8,5 @@ with lcd('backend'):
     local('python main.py /tmp/project1 --build')
 
 with lcd('/tmp/project1/project1'):
+    local('./manage.py check')
     local('./manage.py migrate --run-syncdb')
