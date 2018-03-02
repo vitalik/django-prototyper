db363a145c7059ae5ab2a303a91bd5fe1809f721
Vitaliy Kucheriavyi <ppr.vitaly@gmail.com>
2018-03-02 16:14:39 +0200

    testing prep

---
+++ tests/test.py
@@ -0,0 +1,11 @@
+from fabric.api import local, lcd
+import os
+
+
+local('ls -al /tmp/project1/.djangoprototyper/')
+
+with lcd('backend'):
+    local('python main.py /tmp/project1 --build')
+
+with lcd('/tmp/project1/project1'):
+    local('./manage.py migrate --run-syncdb')
