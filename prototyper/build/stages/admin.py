43fe200bbccea52f765fd7dcd8821af22d690333
Vitaliy Kucheriavyi <ppr.vitaly@gmail.com>
2018-04-05 16:51:03 +0300

    Generating inheritance and admin

--- prototyper/build/stages/admin.py
+++ prototyper/build/stages/admin.py
@@ -8,5 +8,20 @@ class AdminStage(BuildStage):
             self._handle_app(app)
     
     def _handle_app(self, app):
+        contents = ['from django.contrib import admin', 'from .models import *']
+        
+        for model in app['models']:
+            contents.extend(['', ''])
+            contents.extend(self._handle_model(app, model))
+
+        contents.append('')  # empty line
+
         admin_py = Path(self.build.build_path) / app['name'] / 'admin.py'
-        admin_py.write_text('from django.contrib import admin')
+        admin_py.write_text('\n'.join(contents))
+    
+    def _handle_model(self, app, model):
+        return [
+            '@admin.register(%s)' % model['name'],
+            'class %sAdmin(admin.ModelAdmin):' % model['name'],
+            '    pass',
+        ]
